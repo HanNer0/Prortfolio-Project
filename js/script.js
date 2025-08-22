0eca1edb4b7f32d60294d9f1556b216402af83e4
HanNer0 <rhoannekyla.maclang@email.lcup.edu.ph>
2025-08-22 14:05:50 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let menuIcon = document.querySelector("menu-icon");
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelectro('.navbar');
 
-menuIcon.onclick = () =>{
-    menuIcon.classList.toggle('bx-x')
-    navbar.classList.toggle('active')
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x')
+  navbar.classList.toggle('active');
 }

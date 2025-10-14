eb9917b408cc05d87f63d309d5c432c3871639d8
HanNer0 <rhoannekyla.maclang@email.lcup.edu.ph>
2025-10-14 17:28:09 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
 }
+
+for (let i = 0; i < 40; i++) {
+  const dot = document.createElement('span');
+  dot.classList.add('particle');
+  dot.style.left = Math.random() * 100 + 'vw';
+  dot.style.animationDuration = 5 + Math.random() * 10 + 's';
+  dot.style.width = dot.style.height = Math.random() * 6 + 'px';
+  document.querySelector('.background-particles').appendChild(dot);
+}
